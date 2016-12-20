bec8c25625e758e14d79fe7b8d097155e253682d
xuwang <xuwang@snailgame.net>
2016-12-20 13:25:50 +0800

    update 去掉返回id的信息

--- routes/pushurl.js
+++ routes/pushurl.js
@@ -7,24 +7,23 @@ router.post('/login',function(req,res){
   var name = req.body ? req.body.username : null;
   var pwd = req.body ? req.body.pwd : null;
   if(!name || !pwd){return res.status(400).send({ec:400,msg:"login failed for name or pwd == null."});}
-  room.login(name,pwd,function(err,info){
+  room.login(name,pwd,function(err,token){
     if(err){
-      res.status(400).send({ec:400,err:err,msg:"login failed."});
+      res.status(400).send({ec:400,msg:err.message});
     }
     else {
-      res.status(200).send({ec:0,msg:"login success.",info:info});
+      res.status(200).send({ec:0,msg:"login success.",token:token});
     }
   });
 });
 
 router.post('/logout',function(req,res){
   res.header("Access-Control-Allow-Origin", "*");
-  var id = req.body ? req.body.id : null;
   var token = req.body ? req.body.token : null;
-  if(!id || !token){return res.status(400).send({ec:400,msg:"logout failed for id or token == null."});}
-  room.logout(id,token,function(err){
+  if(!token){return res.status(400).send({ec:400,msg:"logout failed for token == null."});}
+  room.logout(token,function(err){
     if(err){
-      res.status(400).send({ec:400,err:err,msg:"logout failed."});
+      res.status(400).send({ec:400,msg:err.message});
     }
     else {
       res.status(200).send({ec:0,msg:"logout success."});
@@ -34,12 +33,11 @@ router.post('/logout',function(req,res){
 
 router.post('/geturl',function(req,res){
   res.header("Access-Control-Allow-Origin", "*");
-  var id = req.body ? req.body.id : null;
   var token = req.body ? req.body.token : null;
-  if(!id || !token){return res.status(400).send({ec:400,msg:"geturl failed for id or token == null."});}
-  room.geturl(id,token,function(err,pushUrl){
+  if(!token){return res.status(400).send({ec:400,msg:"geturl failed for token == null."});}
+  room.geturl(token,function(err,pushUrl){
     if(err){
-      res.status(400).send({ec:400,err:err,msg:"geturl failed."});
+      res.status(400).send({ec:400,msg:err.message});
     }
     else {
       res.status(200).send({ec:0,msg:"geturl success.",pushUrl:pushUrl});
--- snailcloud/room.js
+++ snailcloud/room.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var api = require('./api');
 var mysql = require('mysql');
 var config = require('../config/config');
 var pool = mysql.createPool(config.db_mysql);
 
 function login(name,pwd,cb){
-  if(!name || !pwd){return cb(new Error("name or pwd == null"));}
+  if(!name || !pwd){return cb(new Error("login name or pwd == null"));}
   pool.getConnection(function(err,connection){
     if(err){
 			console.log(err);
@@ -15,7 +14,7 @@ function login(name,pwd,cb){
 		else{
 			console.log('connected as id ' + connection.threadId);
 			//查找用户信息
-      var sql = 'SELECT backinfo.id,token FROM backinfo,user WHERE name = "' + name +
+      var sql = 'SELECT backinfo.id FROM backinfo,user WHERE name = "' + name +
                   '" AND pwd = "' + pwd + '" AND user.id = backinfo.id;';
 			connection.query(sql, function(err, rows, fields) {
 			  if (err) {
@@ -31,6 +30,7 @@ function login(name,pwd,cb){
         }
         else {
           var token = Math.round(Math.random() * 1000000000000);
+          token = token.toString() + '-' + rows[0].id.toString();
           var setSql = 'UPDATE backinfo SET token = "' + token + '",status = 1 WHERE id = "' + rows[0].id + '";';
           connection.query(setSql,function(err,result){
             if(err){
@@ -38,8 +38,7 @@ function login(name,pwd,cb){
               cb(err);
             }
             else {
-              rows[0].token = token.toString();
-              cb(null,rows[0]);
+              cb(null,token);
             }
             //释放连接,pool目前可以用end替代release释放，后续版本则会遗弃这种做法，所以建议用release
     				connection.release();
@@ -50,8 +49,8 @@ function login(name,pwd,cb){
   });
 }
 
-function logout(id,token,cb){
-  if(!id || !token){return cb(new Error("logout failed for id or token == null."));}
+function logout(token,cb){
+  if(!token){return cb(new Error("logout failed for token == null."));}
   pool.getConnection(function(err,connection){
     if(err){
       console.log(err);
@@ -61,17 +60,17 @@ function logout(id,token,cb){
       console.log('connected as id ' + connection.threadId);
 			//更改登录用户信息
       var sql = 'UPDATE backinfo SET token = null,status = 0 WHERE id IN(' +
-      'SELECT id FROM (SELECT id FROM backinfo WHERE id = "' + id + '" AND token = "' + token + '") AS temTable);';
+      'SELECT id FROM (SELECT id FROM backinfo WHERE token = "' + token + '") AS temTable);';
       connection.query(sql,function(err,result){
         if(err){
           console.log(err);
           cb(err);
         }
         else if((result.affectedRows == 1) && (result.changedRows == 1)){
-          cb(null);
+          cb(null);//登出成功
         }
-        else {
-          cb(new Error("logout failed for not unique or not exist."));
+        else {//登出失败
+          cb(new Error("logout failed for not unique or not exist or wrong token."));
         }
         //释放连接,pool目前可以用end替代release释放，后续版本则会遗弃这种做法，所以建议用release
 				connection.release();
@@ -80,8 +79,8 @@ function logout(id,token,cb){
   });
 }
 
-function geturl(id,token,cb){
-  if(!id || !token){return cb(new Error("geturl failed for id or token == null."));}
+function geturl(token,cb){
+  if(!token){return cb(new Error("geturl failed for token == null."));}
   pool.getConnection(function(err,connection){
     if(err){
       console.log(err);
@@ -90,14 +89,14 @@ function geturl(id,token,cb){
     else {
       console.log('connected as id ' + connection.threadId);
 			//匹配该用户是否已经登录是否有权限
-      var sql = 'SELECT id FROM backinfo WHERE id = "' + id + '" AND token = "' + token + '" AND status = 1;';
+      var sql = 'SELECT id FROM backinfo WHERE token = "' + token + '" AND status = 1;';
       connection.query(sql, function(err, rows, fields) {
         if(err){
           console.log(err);
           cb(err);
         }
         else if(rows.length != 1){
-          cb(new Error("geturl failed for not exist user or not login."));
+          cb(new Error("geturl failed for not exist user or not login or wrong token."));
         }
         else {
           //获取该用户为主播所在房间的推流地址
