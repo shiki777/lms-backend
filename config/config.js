6710470e3121dcd6f587167345ca7e7514199f32
mawei <mwei@snailgame.net>
2016-12-23 11:05:09 +0800

    增加pushstream ip port 配置

--- config/config.js
+++ config/config.js
@@ -1,16 +1,20 @@
-const config = {
-    snail_cloud : {
-        host : '192.168.5.233',
-        port : 8999
-    },
-    /*mysql访问*/
-    db_mysql : {
-        			host     : '192.168.5.226',
-        			port     : '3306',
-        			user     : 'lms',
-        			password : 'lms123',
-        			database : 'lms'
-    		    }
-}
+var config = {
+  snail_cloud: {
+    host: '192.168.5.233',
+    port: 8999
+  },
+  /*mysql访问*/
+  db_mysql: {
+    host: '192.168.5.226',
+    port: '3306',
+    user: 'lms',
+    password: 'lms123',
+    database: 'lms'
+  },
+  push_stream_url:{
+    host:'127.0.0.1',
+    port:6969,
+  },
+};
 
 module.exports = config;
