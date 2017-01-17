6202add42120de95c4de2c33b01e78922d6c5cdd
songtao <songtao@snailgame.net>
2017-01-17 15:08:40 +0800

    update log4js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var multer = require('multer');
 var multerupload = multer({ dest: './upload' });
+var log4js = require('log4js');
 
 var app = express();
 app.use(session({
@@ -85,6 +86,5 @@ app.use(function(err, req, res, next) {
     title : '错误页'
   });
 });
-
 
 module.exports = app;
